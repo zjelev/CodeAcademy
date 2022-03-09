22b59c57b78cbfe2b709389bae53b9c5467f5486
zjelev <zlatomir@gmail.com>
2022-03-09 19:57:42 +0200

    2nd

---
+++ 2/MySecondProject/src/math/Mathematics.java
@@ -0,0 +1,10 @@
+package math;
+
+public class Mathematics {
+    public static void main(String[] args) {
+        String myName = "Zlati";
+        System.out.println((Math.PI  + "\n" + myName));
+        System.out.println((Math.PI  + "\n" + myName));
+        System.out.println((int)Math.pow(5, 2));
+    }
+}
