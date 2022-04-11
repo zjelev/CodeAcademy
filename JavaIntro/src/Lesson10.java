c6c0859b5996f534eda849dd0f13a393e4a9ac6a
zjelev <zlatomir@gmail.com>
2022-04-11 13:18:50 +0300

    Update Lesson10.java

--- JavaIntro/src/Lesson10.java
+++ JavaIntro/src/Lesson10.java
@@ -2,13 +2,42 @@ import java.util.Scanner;
 
 public class Lesson10 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        for (int i = 1; i <= 10; i++) {
-            if (i >= 4 && i <= 6) { // i != 5
-                continue;
+        // long currentTime = System.currentTimeMillis();
+        // long nanoTime = System.nanoTime();
+        // System.out.println("Current start time is: " + currentTime);
+        // System.out.println("Current nano time is: " + nanoTime);
+
+        // int count = 0;
+
+        // do {
+        // count++;
+        // if (count % 1000000 == 0) {
+        // System.out.println(count);
+        // }
+        // } while (count < 1_000_000_000);
+
+        // long currentTimeStop = System.currentTimeMillis();
+        // long nanoTimeStop = System.nanoTime();
+
+        // System.out.println("Current stop time is: " + currentTimeStop);
+        // System.out.println("Current stop nano time is: " + nanoTimeStop);
+
+        // System.out.println("currentTimeMillis measure " + (currentTimeStop -
+        // currentTime) + " milliseconds");
+        // System.out.println("nanoTime measure " + (nanoTimeStop - nanoTime)/1000000 +
+        // " milliseconds");
+
+        for (byte i = 1; i <= 6; i++) {
+            System.out.println("outer iteration " + i);
+            for (int j = 0; j <= 5; j++) {
+                if (j <= 2 || j >= 4) { // i != 5
+                    continue;
+                }
+                System.out.print(j + " ");
             }
-            System.out.println(i);
+            System.out.println();
         }
     }
 }
