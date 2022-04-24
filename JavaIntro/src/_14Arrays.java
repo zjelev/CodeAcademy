f66c90f5e9fb8c31d938c1cd4d22ce895faa0290
zjelev <zlatomir@gmail.com>
2022-04-24 21:29:00 +0300

    14. Arrays completed

--- JavaIntro/src/_14Arrays.java
+++ JavaIntro/src/_14Arrays.java
@@ -6,8 +6,8 @@ public class _14Arrays {
         Scanner scan = new Scanner(System.in);
 
         // Lection
-
-        double[] original = { 15, 13.2, -6, 2.36, 20.6, 12.58, -6, 2.36, -6, 568.32, 12.5 };
+        double[] original = { 15, 13.2, 0.000125, -6, 2.36, 12345.654987, 20.6, 12.58, -6,
+                2.36, -6, 568.32, 12.5, 0, -100000, 12345.654987, 0.000125, 15 };
         System.out.println(Arrays.toString(original));
         // System.out.println(calcAverage(original));
 
@@ -26,109 +26,164 @@ public class _14Arrays {
         // System.out.println(Arrays.toString(math));
         // System.out.println("Average math grade is " + calcAverage(math));
 
-        // Java ArrayList == C# List
-
         // Homework
-        // // 1
-        // System.out.println("Input value to be removed: ");
-        // double toRemove = scan.nextDouble();
+        // 1 - Remove certain value
+        System.out.print("Task 1 - Input value to be removed: ");
+        double toRemove = scan.nextDouble();
+        double[] removedValue = removeValue(original, toRemove);
+        System.out.println(Arrays.toString(removedValue));
 
-        // int removeIndex = -1;
-        // for (int i = 0; i < original.length; i++) {
-        // if (original[i] == toRemove) {
-        // removeIndex = i;
-        // }
-        // }
+        // 2 - Insert value at certain index
+        System.out.print("Task 2 - Input value to be inserted: ");
+        double toInsert = scan.nextDouble();
+        System.out.print("Input index to be inserted on: ");
+        int onIndex = scan.nextInt();
+        double[] oneInserted = insertValue(original, toInsert, onIndex);
+        System.out.println(Arrays.toString(oneInserted));
 
-        // double[] oneValueRemoved = new double[original.length - 1];
+        // 3 - Remove duplicate values
+        double[] noDuplicates = removeDuplicates(original);
+        System.out.printf("The array without duplicates is: %s\n",
+                Arrays.toString(noDuplicates));
 
-        // for (int i = 0; i < removeIndex; i++) {
-        // oneValueRemoved[i] = original[i];
-        // }
-        // for (int i = removeIndex; i < original.length - 1; i++) {
-        // oneValueRemoved[i] = original[i + 1];
-        // }
-
-        // // Raboti samo ako removeIndex e pred-pred-poslednia
-        // // for (int i = 0; i <= oneValueRemoved.length - 1; i++) {
-        // // if (i == removeIndex) {
-        // // oneValueRemoved[i] = original[i + 1];
-        // // continue;
-        // // }
-        // // oneValueRemoved[i] = original[i];
-        // // }
-
-        // System.out.println(Arrays.toString(oneValueRemoved));
-
-        // // 2
-        // System.out.println("Input value to be inserted: ");
-        // double toInsert = scan.nextDouble();
-        // System.out.println("Input index to be inserted on: ");
-        // int onIndex = scan.nextInt();
-
-        // if (onIndex < 0 || onIndex > original.length) {
-        // System.out.println("Invalid index");
-        // } else {
-        // double[] oneValueInserted = new double[original.length + 1];
-
-        // for (int i = 0; i < onIndex; i++) {
-        // oneValueInserted[i] = original[i];
-        // }
-        // oneValueInserted[onIndex] = toInsert;
-        // for (int i = onIndex; i < original.length; i++) {
-        // oneValueInserted[i + 1] = original[i];
-        // }
-        // System.out.println(Arrays.toString(oneValueInserted));
-        // }
-
-        // // 3
-        // double[] noDuplicates = new double[original.length];
-        // int index = 0;
-        // for (int i = 0; i < original.length; i++) {
-        //     boolean duplicateFound = false;
-        //     for (int j = i + 1; j < original.length; j++) {
-        //         if (original[i] == original[j]) {
-        //             duplicateFound = true;
-        //             break;
-        //         }
-        //     }
-        //     if (!duplicateFound) {
-        //         noDuplicates[index] = original[i];
-        //         index++;
-        //     }
-        // }
-        // System.out.println(Arrays.toString(noDuplicates));
-
-        // 4
+        // 4 - Find second min/max
         double maxValue = Double.MIN_VALUE;
         double minValue = Double.MAX_VALUE;
         int maxValueIndex = -1;
         int minValueIndex = -1;
-        for (int i = 0; i < original.length; i++) {
-            if (original[i] > maxValue) {
-                maxValue = original[i];
+        double[] firstRemoveDuplicates = removeDuplicates(original);
+        for (int i = 0; i < firstRemoveDuplicates.length; i++) {
+            if (firstRemoveDuplicates[i] > maxValue) {
+                maxValue = firstRemoveDuplicates[i];
                 maxValueIndex = i;
             }
-            if (original[i] < minValue) {
-                minValue = original[i];
+            if (firstRemoveDuplicates[i] < minValue) {
+                minValue = firstRemoveDuplicates[i];
                 minValueIndex = i;
             }
         }
         double secondMaxValue = Double.MIN_VALUE;
         double secondMinValue = Double.MAX_VALUE;
-        int secondMaxValueIndex = -1;
-        int secondMinValueIndex = -1;
-        for (int i = 0; i < original.length; i++) {
-            if (original[i] > secondMaxValue && i != maxValueIndex) {
-                secondMaxValue = original[i];
-                secondMaxValueIndex = i;
+        for (int i = 0; i < firstRemoveDuplicates.length; i++) {
+            if (firstRemoveDuplicates[i] > secondMaxValue && i != maxValueIndex) {
+                secondMaxValue = firstRemoveDuplicates[i];
             }
-            if (original[i] < secondMinValue && i != minValueIndex) {
-                secondMinValue = original[i];
-                secondMinValueIndex = i;
+            if (firstRemoveDuplicates[i] < secondMinValue && i != minValueIndex) {
+                secondMinValue = firstRemoveDuplicates[i];
             }
         }
-        System.out.printf("Second max value is %f and second min value is %f", secondMaxValue, secondMinValue);
+        System.out.printf("Second max value is %f and second min value is %f\n",
+                secondMaxValue, secondMinValue);
+
+        // 5 - The Matrix
+        System.out.print("Input matrix size: ");
+        int size = scan.nextInt();
+        int[][] firstMatrix = new int[size][size];
+        int[][] secondMatrix = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("Input values on row " + i + ", column " + j + " in both matrises: ");
+                firstMatrix[i][j] = scan.nextInt();
+                secondMatrix[i][j] = scan.nextInt();
+            }
+        }
+
+        System.out.println("Printed side by side: ");
+        for (int i = 0; i < size; i++) {
+            System.out.println(Arrays.toString(firstMatrix[i]) + " " + Arrays.toString(secondMatrix[i]));
+        }
+
+        System.out.println("Sum of elements: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print("[");
+            for (int j = 0; j < size - 1; j++) {
+                int sumElement = firstMatrix[i][j] + secondMatrix[i][j];
+                System.out.print(sumElement + ", ");
+            }
+            System.out.printf("%d]\n", firstMatrix[i][size - 1] + secondMatrix[i][size - 1]);
+        }
+
+        System.out.println("Direct sum: ");
+        int[][] directSum = new int[firstMatrix[0].length + secondMatrix[0].length][firstMatrix.length
+                + secondMatrix.length];
+
+        for (int i = 0; i < firstMatrix.length; i++) {
+            for (int j = 0; j < firstMatrix[i].length; j++) {
+                directSum[i][j] = firstMatrix[i][j];
+            }
+        }
+
+        int secondMatrixRow = 0;
+        for (int i = firstMatrix.length; i < directSum.length; i++) {
+            int secondMatrixCol = 0;
+            for (int j = firstMatrix[0].length; j < directSum[i - 1].length; j++) {
+                directSum[i][j] = secondMatrix[secondMatrixRow][secondMatrixCol];
+                secondMatrixCol++;
+            }
+            secondMatrixRow++;
+        }
+
+        for (int[] is : directSum) {
+            System.out.println(Arrays.toString(is));
+        }
+    }
+
+    private static double[] removeDuplicates(double[] original) {
+        double[] noDuplicatesTemp = new double[original.length];
+        int index = 0;
+        for (int i = 0; i < original.length; i++) {
+            boolean duplicateFound = false;
+            for (int j = i + 1; j < original.length; j++) {
+                if (original[i] == original[j]) {
+                    duplicateFound = true;
+                    break;
+                }
+            }
+            if (!duplicateFound) {
+                noDuplicatesTemp[index] = original[i];
+                index++;
+            }
+        }
+        double[] noDuplicates = trimArray(noDuplicatesTemp, index);
+        return noDuplicates;
+    }
+
+    private static double[] trimArray(double[] arr, int index) {
+        double[] trimmedArr = new double[index];
+        for (int i = 0; i < index; i++) {
+            trimmedArr[i] = arr[i];
+        }
+        return trimmedArr;
+    }
+
+    private static double[] insertValue(double[] original, double toInsert, int onIndex) {
+        double[] oneInserted = new double[original.length + 1];
+        if (onIndex < 0 || onIndex > original.length) {
+            System.out.println("Invalid index");
+        } else {
+            for (int i = 0; i < onIndex; i++) {
+                oneInserted[i] = original[i];
+            }
+            oneInserted[onIndex] = toInsert;
+            for (int i = onIndex; i < original.length; i++) {
+                oneInserted[i + 1] = original[i];
+            }
+        }
+        return oneInserted;
+    }
+
+    private static double[] removeValue(double[] original, double value) {
+        double[] removedValueTemp = new double[original.length];
+        int newArrIndex = 0;
+        for (int i = 0; i < original.length; i++) {
+            if (original[i] != value) {
+                removedValueTemp[newArrIndex] = original[i];
+                newArrIndex++;
+            }
+        }
+
+        return trimArray(removedValueTemp, newArrIndex);
     }
 
     private static double calcAverage(double[] original) {
